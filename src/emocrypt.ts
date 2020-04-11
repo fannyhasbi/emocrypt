dd470aacef09a2b953bdd4fce44f0dd74d415c8c
fannyhasbi <fnyhsbi@gmail.com>
2020-04-12 06:14:57 +0700

    create possibleEmojis function
    possible emojis from the given index picks at least 1 emoji that will later be randomize pick from the chosen emojis

--- src/emocrypt.ts
+++ src/emocrypt.ts
@@ -1,6 +1,16 @@
 import { Rabbit, DecryptedMessage } from 'crypto-js';
 
 const emojis: Array<string> = ['😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊', '😇', '😘', '😍', '😘', '😗', '☺', '😚', '😙', '😋', '😛', '😜', '🤪', '😝', '🤑', '🤗', '🤭', '🤫', '🤔', '🤐', '🤨', '😐', '😑', '😶', '😏', '😒', '🙄', '😬', '🤥', '😌', '😔', '😪', '🤤', '😴', '😷', '🤒', '🤕', '🤢', '🤮', '🤧', '🥵', '🥶', '🥴', '😵', '🤯', '🤠', '🥳', '😎', '🤓', '🧐', '😕', '😟', '🙁', '☹', '😮', '😯'];
+const emojisLength = emojis.length;
+const maxChars = 65; // base64 characters count
+
+function possibleEmojis(index: number): Array<string>{
+  let possibilities: Array<string> = [];
+  for(let i = 0; i < emojisLength; i += maxChars+index){
+    possibilities.push(emojis[index]);
+  }
+  return possibilities
+}
 
 function encrypt(message: string, key: string): string {
   return Rabbit.encrypt(message, key).toString();
@@ -12,6 +22,7 @@ function decrypt(cipher: string, key: string) : DecryptedMessage {
 
 export {
   emojis,
+  possibleEmojis,
   encrypt,
   decrypt,
 };
--- test/emocrypt.test.ts
+++ test/emocrypt.test.ts
@@ -1,6 +1,20 @@
 import { assert } from 'chai';
 import { enc } from 'crypto-js';
-import { emojis, encrypt, decrypt } from '../src/emocrypt';
+import {
+  emojis,
+  encrypt,
+  decrypt,
+  possibleEmojis,
+} from '../src/emocrypt';
+
+describe('Encryption prelogic', () => {
+  it('should return more than 0', () => {
+    assert.isAtLeast(possibleEmojis(10).length, 1);
+    assert.isAtLeast(possibleEmojis(20).length, 1);
+    assert.isAtLeast(possibleEmojis(30).length, 1);
+    assert.isAtLeast(possibleEmojis(40).length, 1);
+  });
+});
 
 describe('Initial crypto', () => {
   it('should return string', () => {
@@ -21,4 +35,4 @@ describe('Emojis', () => {
     console.log('emojis count :', emojis.length);
     assert.isAbove(emojis.length, 1);
   })
-})
+});
